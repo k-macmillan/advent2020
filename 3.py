5481e5c3084d8efb35aab55af4f0aa108ae20fc8
Kyle MacMillan <kyle.w.macmillan@gmail.com>
2020-12-03 01:51:21 -0500

    Made more versatile. Removed hard-coded values.

--- 3.py
+++ 3.py
@@ -6,21 +6,22 @@ def main():
         # Strip the first line out because it doesn't matter
         tree_map = [line.rstrip('\n') for line in f]
 
-    total_trees = trees_hit(tree_map, 1, 1)
-    total_trees *= trees_hit(tree_map, 3, 1)
-    total_trees *= trees_hit(tree_map, 5, 1)
-    total_trees *= trees_hit(tree_map, 7, 1)
-    total_trees *= trees_hit(tree_map, 1, 2)
+    total_trees = trees_hit(tree_map, 1, 1, '#')
+    total_trees *= trees_hit(tree_map, 3, 1, '#')
+    total_trees *= trees_hit(tree_map, 5, 1, '#')
+    total_trees *= trees_hit(tree_map, 7, 1, '#')
+    total_trees *= trees_hit(tree_map, 1, 2, '#')
     print(total_trees)
 
 
-def trees_hit(tree_map, right, down):
+def trees_hit(tree_map, right, down, symbol):
     tree_count = 0
+    length = len(tree_map[0])
     for i, line in enumerate(tree_map[::down]):
-        if line[(i * right) % 31] == '#':
+        if line[(i * right) % length] == symbol:
             tree_count += 1
 
-    if tree_map[0][0] == '#':
+    if tree_map[0][0] == symbol:
         tree_count -= 1
 
     return tree_count
